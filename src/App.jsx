d2491df9e6b3691ae4d3af75e6f42351a77ec54d
Robin James Kerrison <rjk1994@gmail.com>
2018-07-23 12:57:17 +0100

    Starting router work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import Image from './Image.jsx'
+import { Link, Route } from 'react-router-dom'
 
 class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/rabbits'>Rabbits</Link>
+        </div>
+        <div>
+          <Route to='/'>
+            <BobButton />
+          </Route>
+          <Route to='/rabbits'>
+            <Rabbits {...this.props} />
+          </Route>
+        </div>
+      </div>
+    )
+  }
+}
+
+class BobButton extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -19,12 +41,21 @@ class App extends React.Component {
     return (
       <div>
         <p>{this.state.text}</p>
-        <Image titleOfTheImage={this.state.text} />
+        <button onClick={this.changeTheTextToBob.bind(this)}>Change the text to Bob</button>
+      </div>
+    )
+  }
+}
+
+class Rabbits extends React.Component {
+  render() {
+    return (
+      <div>
+        <Image titleOfTheImage='{this.state.text}' />
         <Image titleOfTheImage="Another rabbit!" />
         <Image titleOfTheImage="A third rabbit!" />
         <Image />
         <Image titleOfTheImage="The final rabbit." />
-        <button onClick={this.changeTheTextToBob.bind(this)}>Change the text to Bob</button>
       </div>
     )
   }
